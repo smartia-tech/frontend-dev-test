98b8b5419df24bb02cf93be3192441f572862e1b
Lee <>
2021-03-10 19:20:17 -0800

    Raw result and data displayed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function AppError(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="error">
+      Something happened: { props.error }
     </div>
   );
+}
+
+function AppLoading(props) {
+  return (
+    <div className="loading">
+      Loading apps
+    </div>
+  )
+}
+
+function Launch(props) {
+  return (
+    <div className="launch">
+      {props.launch.name}<br/>
+      <img className="launch-patch" src={props.launch.links.patch.small} alt="Launch icon"/><br/>
+      Date: {props.launch.date_utc}<br/>
+      Success: {props.launch.cores.landing_success}
+    </div>
+  )
+}
+
+function Launches(props) {
+  return (
+    <div className="launches">
+      launches: {props.launches.length}
+      {props.launches.map(launch => <Launch launch={launch} key={launch.id}/>)}
+    </div>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      launches: [],
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    const endpoint = 'https://api.spacexdata.com/v4/launches';
+    (async () => {
+      this.setState({
+        launches: await (await fetch(endpoint)).json(),
+        loaded: true
+      });
+    })().catch(ex => {
+      this.setState({
+        error: ex
+      });
+    })
+  }
+
+  render() {
+    if (this.state.error) return <AppError error={this.state.error}/>;
+    if (!this.state.loaded) return <AppLoading/>;
+    return <Launches launches={this.state.launches}/>;
+  }
 }
 
 export default App;
