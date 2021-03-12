dcc1e16d368a9a3cdb75c59f13badee9e2633f01
Martins Akeredolu <olamideakeredolu@gmail.com>
2021-03-12 10:06:10 +0100

    Completed project. Used axios for data fetching and sass for styling. Completed in about 2 or 3 hours

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [launches, setLaunches] = useState([]);
+  const [launchSearch, setLaunchSearch] = useState('');
+  const [launchList, setLaunchList] = useState(launches);
+  const [currentLaunch, setCurrentLaunch] = useState(undefined);
+
+  const fetchLaunches = async () => {
+    const myLaunches = await axios(
+      'https://api.spacexdata.com/v4/launches/past'
+    );
+
+    setLaunches(myLaunches.data);
+  };
+
+  //get launches on render
+  useEffect(() => fetchLaunches(), []);
+
+  //create list of launches
+  const allLaunches = launchList.map((launch) => (
+    <div
+      className="pastLaunch"
+      key={launch.id}
+      onClick={() => setCurrentLaunch(launch)}
+    >
+      <span>{launch.name}</span>
     </div>
+  ));
+
+  //search for launch
+  useEffect(() => {
+    const newList = launches.filter((el) =>
+      el.name.toLowerCase().startsWith(launchSearch.toLowerCase())
+    );
+    setLaunchList(newList);
+  }, [launchSearch, launches]);
+
+  return (
+    <main>
+      <div className="search">
+        <input
+          type="text"
+          value={launchSearch}
+          onChange={(e) => setLaunchSearch(e.target.value)}
+          placeholder="Name"
+        />
+      </div>
+      <div className="content">
+        <section className="left">
+          {currentLaunch === undefined ? (
+            <p>Select Launch</p>
+          ) : (
+            <>
+              <div className="launchImage">
+                <img
+                  src={currentLaunch.links.patch.small}
+                  alt={currentLaunch.name}
+                />
+              </div>
+              <h3>{currentLaunch.name}</h3>
+              <h4>
+                {new Date(currentLaunch.date_local).toLocaleString('en-US', {
+                  dateStyle: 'medium',
+                  timeStyle: 'short'
+                })}
+              </h4>
+              <h5
+                style={
+                  currentLaunch.success
+                    ? { color: 'green' }
+                    : { color: 'darkred' }
+                }
+              >
+                {currentLaunch.success ? 'SUCCESS' : 'FAILED'}
+              </h5>
+            </>
+          )}
+        </section>
+        <section className="right">
+          {launchList.length < 1 ? <p>No launch found</p> : allLaunches}
+        </section>
+      </div>
+    </main>
   );
 }
 
