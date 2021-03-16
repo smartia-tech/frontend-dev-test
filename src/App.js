70e8ff351a4975cfc9f44c80e8c2f91c27a49d3b
Justin Blaisdell <jsblaisdell@gmail.com>
2021-03-16 18:26:15 -0500

    Build and style SpaceX launch list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
+  const [allLaunches, setAllLaunches] = useState([]);
+  const [displayedLaunches, setDisplayedLaunches] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.spacexdata.com/v4/launches/past')
+      .then(response => {
+        if (!response.ok) { throw response }
+        return response.json();
+      })
+      .then(data => {
+        setAllLaunches(data);
+        setDisplayedLaunches(data);
+      }).catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  function filterResults(filter) {
+    const filteredData = allLaunches.filter(launch => (
+      launch.name.toLowerCase().includes(filter)
+    ));
+    setDisplayedLaunches(filteredData);
+  };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-wrapper">
+      <form className="filter__form" action="" onSubmit={handleSubmit}>
+        <label className="filter__label" htmlFor="filter">Filter</label>
+        <input
+          className="filter__input"
+          id="filter"
+          type="text"
+          onChange={(e) => filterResults(e.target.value)}
+        />
+      </form>
+
+      <ul className="results__list">
+        {displayedLaunches.map(launch => (
+          <li key={launch.id} className="result__item">
+            <a className="result__link" href={launch.links.wikipedia}>
+              <img
+                className="result__patch"
+                src={launch.links.patch.small}
+                alt={`${launch.name} patch`}
+              />
+              <div className="result__name">{launch.name}</div>
+              <div className="result__date">
+                {new Date(launch.date_local).toDateString()}
+              </div>
+              <ul className="result__cores">
+                {launch.cores.map(core => (
+                  <li
+                    className={`result__core-status
+                      result__core-status--${core.landing_success ? 'success' : 'failure'}`}
+                    key={core.core}
+                  >
+                    {core.landing_success ? 'Success' : 'Failure'}
+                  </li>
+                ))}
+              </ul>
+            </a>
+          </li>
+        )).reverse()}
+      </ul>
     </div>
   );
 }
