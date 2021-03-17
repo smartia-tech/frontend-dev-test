cedc4ffb5135b06dab3f7e4762b069455020b87a
teekaytech <taofeek.olalere@feghas.com>
2021-03-17 12:38:51 +0100

    Configure eslint and stylelint linters

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p>Hello world</p>
+        <p>Hello world</p>
       </header>
     </div>
   );
