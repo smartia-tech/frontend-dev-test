47904efeacb54bdffbf6201fedc922d4e318c278
Gabriel Rosa <gabrieldsrosa@gmail.com>
2021-03-18 21:56:04 -0300

    feat: update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders application title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/SpaceX's Launches/i);
   expect(linkElement).toBeInTheDocument();
 });
