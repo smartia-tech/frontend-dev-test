610289bb443779fd592f45c6106688aef81c8cf0
Gabriel Rosa <gabrieldsrosa@gmail.com>
2021-03-18 21:36:43 -0300

    feat: implement api integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import useSWR from 'swr';
+
+import LaunchesList from './components/LaunchesList';
+import LaunchesSearch from './components/LaunchesSearch';
+
+import parser from './helpers/parser';
+import fetcher from './helpers/fetcher';
+import filterLaunches from './helpers/searchFilter';
 
 function App() {
+  const { search } = window.location;
+  const query = new URLSearchParams(search).get('launch');
+
+  const [hasError, setHasError] = useState(null);
+  const [period, setPeriod] = useState('past');
+  const [searchQuery, setSearchQuery] = useState(query || '');
+  const [launches, setLaunches] = useState(null);
+
+  const { data, error } = useSWR(`https://api.spacexdata.com/v4/launches/${period}`, fetcher);
+
+  const allLaunches = parser.get(launches);
+  const filteredLaunches = filterLaunches(allLaunches, searchQuery);
+
+  useEffect(() => {
+    setLaunches(data);
+    setHasError(error);
+  }, [data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <h1 className="content__title">SpaceX&apos;s Launches</h1>
+      {hasError && <h1>Ooops, we cound&apos;t process the request :( </h1>}
+      <div className="content__actions">
+        <LaunchesSearch
+          handlePeriodChange={(e) => setPeriod(e.target.value)}
+          handleSearchChange={(e) => setSearchQuery(e.target.value)}
+          period={period}
+          searchQuery={searchQuery}
+        />
+      </div>
+      {!hasError && !launches && <h1>Loading...</h1>}
+      {launches && (
+        <div className="grid">
+          <LaunchesList launches={filteredLaunches} />
+        </div>
+      )}
     </div>
   );
 }
